3d16f2c5ffd20b063988c4841319dee29d3b5cb4
er1chu <mail@erichu.info>
2022-09-09 14:47:07 -0400

    perf:

--- src/server/routers/uses.ts
+++ src/server/routers/uses.ts
@@ -1,11 +1,11 @@
-import { usesResultsApi } from "../../common/api";
-import { zodiosRouter } from "@zodios/express";
+import { usesResultsApi } from '../../common/api'
+import { zodiosRouter } from '@zodios/express'
 
-export const usesRouter = zodiosRouter(usesResultsApi);
+export const usesRouter = zodiosRouter(usesResultsApi)
 
-const baseUrl = `https://fontsinuse.com/api/1/uses.json?api_key=${process.env.API_KEY}&app_name=${process.env.API_APP_ID}`;
-usesRouter.get("/uses", async (req, res) => {
-  const uses = await fetch(baseUrl).then((response) => response.json());
-  res.setHeader("Cache-Control", "s-maxage=10000, stale-while-revalidate=3600");
-  res.status(200).json(uses);
-});
+const baseUrl = `https://fontsinuse.com/api/1/uses.json?&order=modified&api_key=${process.env.API_KEY}&app_name=${process.env.API_APP_ID}`
+usesRouter.get('/uses', async (req, res) => {
+  const uses = await fetch(baseUrl).then((response) => response.json())
+  res.setHeader('Cache-Control', 's-maxage=10000, stale-while-revalidate=3600')
+  res.status(200).json(uses)
+})
