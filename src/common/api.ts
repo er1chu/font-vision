9bf8faf259e9839b5a1a9fbb3d434534e3baa4d7
er1chu <mail@erichu.info>
2022-09-09 14:46:12 -0400

    fix: :bug: fixed types resulting in failed parsing

--- src/common/api.ts
+++ src/common/api.ts
@@ -9,12 +9,14 @@ const user = z.object({
 })
 
 const GenericFontObject = z.object({
-  id: z.number(),
+  id: z.number().positive(),
   name: z.string(),
   slug: z.string(),
-  use_count: z.number().positive(),
-  permalink: z.string(),
+  use_count: z.number().nonnegative(),
+  permalink: z.string().url(),
 })
+
+const Tag = GenericFontObject.omit({ name: true })
 
 const Format = GenericFontObject
 
@@ -28,10 +30,15 @@ const Contributor = z.object({
   slug: z.string(),
   username: z.string(),
   pretty_name: z.string(),
-  first_name: z.string(),
-  surname: z.string(),
+  first_name: z.optional(z.string()),
+  surname: z.optional(z.string()),
   permalink: z.string(),
 })
+
+const webPUrl = z
+  .string()
+  .url()
+  .transform((val) => val.replace('jpeg', 'webp'))
 
 const Topic = GenericFontObject
 
@@ -44,7 +51,7 @@ export const use = z.object({
   topics: z.array(Topic),
   industries: z.array(GenericFontObject),
   designers: z.array(GenericFontObject),
-  tags: z.array(GenericFontObject),
+  tags: z.array(Tag),
   source_type: z.string(),
   locations: z.array(
     z.object({
@@ -53,18 +60,18 @@ export const use = z.object({
       longitude: z.optional(z.string()),
       name: z.string(),
       slug: z.string(),
-      use_count: z.number().positive(),
+      use_count: z.number().nonnegative(),
     })
   ),
   slug: z.string(),
   modified: z.string(),
   permalink: z.string(),
-  thumb: z.string(),
+  thumb: webPUrl,
 })
 
 export const usesResults = z.object({
   count: z.number(),
-  page_size: z.string(),
+  page_size: z.number(),
   uses: z.array(use),
 })
 
