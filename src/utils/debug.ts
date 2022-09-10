bcc6aa1b57cf130464fa698a92121786ba9b7887
er1chu <mail@erichu.info>
2022-09-10 11:12:04 -0400

    test: :white_check_mark: add static content and debug utils

---
+++ src/utils/debug.ts
@@ -0,0 +1,17 @@
+import { entitiesResults, usesResults } from '../common/api'
+import * as entitiesData from '../lib/sampleData/entities.json'
+import * as usesData from '../lib/sampleData/uses.json'
+
+enum StaticData {
+  'entities' = 0,
+  'uses' = 1,
+  //   'detailedFamilies' = 2,
+}
+
+const dataSets = [entitiesResults.parse(entitiesData), usesResults.parse(usesData)]
+
+const debugData = (setNumber: StaticData): void => {
+  console.log(dataSets[setNumber])
+}
+
+export default debugData
