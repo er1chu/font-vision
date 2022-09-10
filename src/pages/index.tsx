7d812ec2793f4c452b686055898752a2b89db2e5
er1chu <mail@erichu.info>
2022-09-10 19:11:44 -0400

    refactor: :art: display use case

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,12 +14,22 @@ const Uses = () => {
   if (isError) return <div>Error</div>
   return (
     <>
-      {uses?.uses.map((use) => (
-        <>
-          {use.font_families.map((font, index) => (
-            <DisplayUnit name={font.name} sampleSrc={font.sample_src} useCount={font.use_count} key={index} />
-          ))}
-        </>
+      {uses?.uses.map(({ font_families, thumb }, index) => (
+        <div
+          className='group flex aspect-[0.8] flex-col overflow-hidden rounded-lg odd:bg-[#b9ff00] even:bg-[#FFE0F8] hover:cursor-pointer'
+          key={index}>
+          <div>
+            {font_families.slice(0, 3).map(({ name, sample_src, use_count }, index) => (
+              <img className='max-w-full border-b border-black even:bg-white' src={sample_src} alt={name} key={index} />
+            ))}
+          </div>
+          <img
+            className='object-cover opacity-100 group-hover:opacity-20'
+            src={thumb}
+            loading='lazy'
+            alt={`Design featuring ${font_families[0]?.name}`}
+          />
+        </div>
       ))}
     </>
   )
