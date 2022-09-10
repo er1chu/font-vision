1e5d29f965cf8a9c759086f5a0357dad5e260905
er1chu <mail@erichu.info>
2022-09-10 19:12:18 -0400

    chore: :wrench: increase item call

--- src/server/routers/uses.ts
+++ src/server/routers/uses.ts
@@ -3,7 +3,7 @@ import { zodiosRouter } from '@zodios/express'
 
 export const usesRouter = zodiosRouter(usesResultsApi)
 
-const baseUrl = `https://fontsinuse.com/api/1/uses.json?&stack-picks=1&order=created&api_key=${process.env.API_KEY}&app_name=${process.env.API_APP_ID}`
+const baseUrl = `https://fontsinuse.com/api/1/uses.json?count=40&staff-picks=1&order=created&api_key=${process.env.API_KEY}&app_name=${process.env.API_APP_ID}`
 usesRouter.get('/uses', async (req, res) => {
   const uses = await fetch(baseUrl).then((response) => response.json())
   res.setHeader('Cache-Control', 's-maxage=10000, stale-while-revalidate=3600')
