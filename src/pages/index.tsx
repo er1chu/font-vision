1cce7b7d505dbbb305a49c29e62ff642cfdb4712
er1chu <mail@erichu.info>
2022-09-11 17:57:34 -0400

    refactor: :dizzy: refactor scroll animation to use a ref

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,22 @@
 import type { NextPage } from 'next'
+import type { FontFamily } from '../common/api'
+import type { RefObject, MutableRefObject } from 'react'
+
 import { Zodios } from '@zodios/core'
 import { ZodiosHooks } from '@zodios/react'
 import { usesResultsApi } from '../common/api'
 import Layout from '../components/Layout'
-import DisplayUnit from '../components/DisplayUnit'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { scroll, animate, stagger } from 'motion'
-import { z } from 'zod'
+import { useRef } from 'react'
+interface FontUseProps {
+  useIndex: number
+  fontFamilies: Array<FontFamily>
+  thumb: string
+}
 
 const usesClientApi = new Zodios('/api', usesResultsApi)
 const usesClientHooks = new ZodiosHooks('uses', usesClientApi)
-
-const Uses = () => {
-  useEffect(() => {
-    document.querySelectorAll('.family-unit').forEach((target) => {
-      scroll(animate(target, { scale: [0.5, 1, 1, 0.5] }, { delay: 0.2 }), {
-        target: target,
-        offset: ['start end', 'end end', 'start start', 'end start'],
-      })
-    })
-  })
-  const { data: uses, isLoading, isError } = usesClientHooks.useGetUses()
-  if (isLoading) return <div className='min-w-screen min-h-screen bg-green-100'>Loading Fonts...</div>
-  if (isError) return <div>Error</div>
-  return (
-    <>
-      {uses?.uses.map(({ font_families, thumb }, useIndex) => (
-        <div
-          className='family-unit group flex aspect-[0.8] flex-col overflow-hidden rounded-lg odd:bg-[#b9ff00] even:bg-[#FFE0F8] hover:cursor-pointer'
-          key={useIndex}>
-          <div>
-            {font_families.slice(0, 3).map(({ name, sample_src, use_count }, fontIndex) => (
-              <img
-                className='max-w-full border-b border-black even:bg-white'
-                src={sample_src}
-                alt={name}
-                key={fontIndex}
-                loading={useIndex < 10 ? 'eager' : 'lazy'}
-              />
-            ))}
-          </div>
-          <img
-            className='group-hover:opacity-2 object-cover opacity-100 transition-all'
-            src={thumb}
-            loading='lazy'
-            alt={`Design featuring ${font_families[0]?.name}`}
-          />
-        </div>
-      ))}
-    </>
-  )
-}
 
 const UsesResults: NextPage = () => {
   return (
@@ -60,4 +26,60 @@ const UsesResults: NextPage = () => {
   )
 }
 
+const Uses = () => {
+  const { data: uses, isLoading, isError } = usesClientHooks.useGetUses()
+  if (isLoading) return <div className='min-w-screen min-h-screen bg-green-100'>Loading Fonts...</div>
+  if (isError) return <div>Error</div>
+  return (
+    <>
+      {uses?.uses.map(({ font_families, thumb }, useIndex) => (
+        <FontUseUnit fontFamilies={font_families} thumb={thumb} useIndex={useIndex} key={useIndex} />
+      ))}
+    </>
+  )
+}
+
+const FontUseUnit: React.FC<FontUseProps> = ({ useIndex, fontFamilies, thumb }) => {
+  const animationRef = useRef<Element>(null) as MutableRefObject<Element>
+  useEffect(() => {
+    scroll(
+      animate(
+        animationRef.current,
+        {
+          opacity: [0.5, 1, 1, 1],
+          scale: [0.8, 1, 1, 1],
+          borderRadius: ['10%', '0%', '0%', '0%'],
+        },
+        { easing: 'ease-in-out' }
+      ),
+      {
+        target: animationRef.current,
+        offset: ['start end', 'end end', 'start start', 'end start'],
+      }
+    )
+  })
+  return (
+    <div
+      ref={animationRef as RefObject<HTMLDivElement>}
+      className='family-unit group flex aspect-[0.8] flex-col overflow-hidden rounded-lg odd:bg-[#b9ff00] even:bg-[#FFE0F8] hover:cursor-pointer'>
+      <div>
+        {fontFamilies.slice(0, 3).map(({ name, sample_src, use_count }, fontIndex) => (
+          <img
+            className='max-w-full border-b border-black even:bg-white'
+            src={sample_src}
+            alt={name}
+            key={fontIndex}
+            loading={useIndex < 10 ? 'eager' : 'lazy'}
+          />
+        ))}
+      </div>
+      <img
+        className='group-hover:opacity-2 object-cover opacity-100 transition-all'
+        src={thumb}
+        loading='lazy'
+        alt={`Design featuring ${fontFamilies[0]?.name}`}
+      />
+    </div>
+  )
+}
 export default UsesResults
