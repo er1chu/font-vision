fb58b87f4baefbc1bd36dea5ba13928c3cce226a
er1chu <mail@erichu.info>
2022-09-11 22:11:58 -0400

    chore: :package: update import to new directories

--- src/common/hooks/use-isomorphic-layout-effect.ts
+++ src/common/hooks/use-isomorphic-layout-effect.ts
@@ -1,7 +1,7 @@
 import { useEffect, useLayoutEffect } from 'react'
 
 // useLayoutEffect is not available on the server, so we need a hook to check if we are on the server
-// or not and to use useEffect on the server and useLayoutEffect on the client.
+// or not and to use useLayout
 
 export const useIsomorphicEffect = () => {
   return typeof window !== 'undefined' ? useLayoutEffect : useEffect
