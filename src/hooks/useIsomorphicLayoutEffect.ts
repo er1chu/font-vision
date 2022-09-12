0afb668512b246130bad03533bee2c79fa5e57fd
er1chu <mail@erichu.info>
2022-09-11 20:10:10 -0400

    chore: :bulb: add detailed comment in custom layout hook

--- src/hooks/useIsomorphicLayoutEffect.ts
+++ src/hooks/useIsomorphicLayoutEffect.ts
@@ -1,5 +1,8 @@
 import { useEffect, useLayoutEffect } from 'react'
 
+// useLayoutEffect is not available on the server, so we need a hook to check if we are on the server
+// or not and to use useEffect on the server and useLayoutEffect on the client.
+
 export const useIsomorphicEffect = () => {
   return typeof window !== 'undefined' ? useLayoutEffect : useEffect
 }
