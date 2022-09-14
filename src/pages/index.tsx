396df036b38451b631dc7ecd1e2cc95dcd246985
er1chu <mail@erichu.info>
2022-09-14 02:32:01 -0400

    refactor: :recycle: error boundary and suspense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,16 @@ import { ZodiosHooks } from '@zodios/react'
 import { usesResultsApi } from '@/common/api-types'
 import Layout from '@/components/layout'
 import FontUseUnit from '@/components/font-use-unit'
+import { Suspense } from 'react'
+import { QueryErrorResetBoundary } from 'react-query'
+import { ErrorBoundary } from 'react-error-boundary'
 
 const usesClientApi = new Zodios('/api', usesResultsApi)
 const usesClientHooks = new ZodiosHooks('uses', usesClientApi)
+
+// TODO: 'do page params for uses, get that out of way for infinite query
+// TODO: Infinite query
+// TODO: suspense error boundary
 
 const UsesResults: NextPage = () => {
   return (
@@ -19,8 +26,13 @@ const UsesResults: NextPage = () => {
   )
 }
 
+export async function getStaticProps() {
+  const posts = await fetch('/api/uses')
+  return { props: { posts } }
+}
+
 const Uses: React.FC = () => {
-  const { data: uses, isLoading, isError } = usesClientHooks.useQuery('/uses')
+  const { data: uses, isLoading, isError } = usesClientHooks.useGetUses(undefined, { retry: 0, staleTime: Infinity })
   if (isLoading)
     return (
       <div className='min-w-screen lg:col-span5 col-span-2 flex min-h-screen items-center justify-center bg-green-100 text-7xl md:col-span-3'>
@@ -34,19 +46,32 @@ const Uses: React.FC = () => {
       </div>
     )
   return (
-    <>
-      {uses?.uses.map(({ font_families, thumb, contributor, tags, designers }, useIndex) => (
-        <FontUseUnit
-          fontFamilies={font_families}
-          thumb={thumb}
-          useIndex={useIndex}
-          key={useIndex}
-          contributor={contributor}
-          tags={tags}
-          designers={designers}
-        />
-      ))}
-    </>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary
+          onReset={reset}
+          fallbackRender={({ resetErrorBoundary }) => (
+            <div>
+              There was an error!
+              <button onClick={() => resetErrorBoundary()}>Try again</button>
+            </div>
+          )}>
+          {uses?.uses.map(({ font_families, thumb, contributor, tags, designers }, useIndex) => (
+            // <Suspense fallback={<div>Loading</div>} key={useIndex}>
+            <FontUseUnit
+              fontFamilies={font_families}
+              thumb={thumb}
+              useIndex={useIndex}
+              key={useIndex}
+              contributor={contributor}
+              tags={tags}
+              designers={designers}
+            />
+            // </Suspense>
+          ))}
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
   )
 }
 
