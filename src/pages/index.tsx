60d74e8b1974f3b7992c352c1c629c73cd5e5e8c
er1chu <mail@erichu.info>
2022-09-17 01:08:43 -0400

    feat: :zap: infinite scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,39 +4,60 @@ import type { UsesResults } from '@/common/api-types'
 
 import { Zodios } from '@zodios/core'
 import { ZodiosHooks } from '@zodios/react'
-
 import { usesResultsApi, usesResults as usesResultsValidation } from '@/common/api-types'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { useRef, useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
+
 import Layout from '@/components/layout'
 import FontUseUnit from '@/components/font-use-unit'
-import { QueryErrorResetBoundary, useQuery, QueryClientProvider, QueryClient } from 'react-query'
-import { ErrorBoundary } from 'react-error-boundary'
 import LoadingScreen from '@/components/loading-screen'
 
 const usesClientApi = new Zodios('/api', usesResultsApi)
 const usesClientHooks = new ZodiosHooks('uses', usesClientApi)
 
-// TODO: 'do page params for uses, get that out of way for infinite query
-// TODO: Infinite query
-// TODO: suspense error boundary
-
 const UsesResults: NextPage = () => {
-  // console.log(usesInitial)
   return (
     <Layout>
       <Uses />
+      <ReactQueryDevtools initialIsOpen={false} />
     </Layout>
   )
 }
 
 const Uses: React.FC = () => {
+  const { ref, inView } = useInView()
+  const page = useRef(1)
   const {
     data: uses,
     isLoading,
     isError,
-  } = useQuery('/uses', usesClientApi.getUses, {
-    staleTime: Infinity,
-    retry: 0,
-  })
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = usesClientHooks.useInfiniteQuery(
+    '/uses',
+    { queries: { page: 1, count: 30 }, retry: 0, staleTime: Infinity },
+    {
+      getPageParamList: () => {
+        return ['page']
+      },
+      getNextPageParam: () => {
+        return {
+          queries: {
+            page: page.current + 1,
+          },
+        }
+      },
+    }
+  )
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage()
+      page.current += 1
+    }
+  }, [inView, fetchNextPage])
+
   if (isLoading) return <LoadingScreen unitCount={30} />
   if (isError)
     return (
@@ -46,17 +67,34 @@ const Uses: React.FC = () => {
     )
   return (
     <>
-      {uses?.uses.map(({ font_families, thumb, contributor, tags, designers }, useIndex) => (
-        <FontUseUnit
-          fontFamilies={font_families}
-          thumb={thumb}
-          useIndex={useIndex}
-          key={useIndex}
-          contributor={contributor}
-          tags={tags}
-          designers={designers}
-        />
-      ))}
+      {uses?.pages.flatMap((page) =>
+        page?.uses.map(({ font_families, thumb, contributor, tags, designers }, useIndex) => (
+          <FontUseUnit
+            fontFamilies={font_families}
+            thumb={thumb}
+            useIndex={useIndex}
+            key={useIndex}
+            contributor={contributor}
+            tags={tags}
+            designers={designers}
+          />
+        ))
+      )}
+      <div
+        ref={ref}
+        onClick={() => {
+          fetchNextPage()
+          page.current = page.current + 1
+        }}
+        className='col-span-5 flex items-center justify-center bg-green-500 p-20 text-7xl'>
+        {isFetchingNextPage ? (
+          <div className='animate-pulse'>Loading more fonts...</div>
+        ) : hasNextPage ? (
+          'Load more fonts'
+        ) : (
+          'No more fonts :('
+        )}
+      </div>
     </>
   )
 }
