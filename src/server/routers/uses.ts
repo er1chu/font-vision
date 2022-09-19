49ad530749be4e6d01514e91713e50f12e600a66
er1chu <mail@erichu.info>
2022-09-19 00:41:38 -0400

    feat: :card_file_box: better query params

--- src/server/routers/uses.ts
+++ src/server/routers/uses.ts
@@ -8,7 +8,7 @@ const baseUrl = `https://fontsinuse.com/api/1/uses.json`
 usesRouter.get('/uses', async (req, res) => {
   const query = req.query
   const { page, count } = query as { page: number; count: number }
-  const requestUrl = `${baseUrl}?page=${page || 1}&count=${count || 30}&staff-picks=1&order=created&api_key=${
+  const requestUrl = `${baseUrl}?page=${page || 1}&count=${count || 30}&order=created&api_key=${
     process.env.API_KEY
   }&app_name=${process.env.API_APP_ID}`
   const uses = await fetch(requestUrl).then((response) => response.json())
